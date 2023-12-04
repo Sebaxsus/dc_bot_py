1110584a9f74c03011b09be5a48303871953e492
Sebaxsus <sebastiangarcia1198@gmail.com>
2023-12-03 19:07:51 -0500

    eliminado del branch archivos con info basura y sensible

--- tarea.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(num, ant, cont):
-    if(cont > 1):
-        aux = ant
-        num = ant + num
-        ant = aux + num
-        print(num, ant)
-        fibonacci(num, ant, (cont - 1))
-    
-fibonacci(1,0,7)
